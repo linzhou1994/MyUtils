cf3d435dd5e736aee770e373cca47b9e632abafe
linzhou <970060731@qq.com>
2018-10-21 23:41:02 +0800

    MyUtils

---
+++ src/main/java/com/my/utils/jsonutils/JsonUtil.java
@@ -0,0 +1,79 @@
+package com.my.utils.jsonutils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * 佛曰:
+ * 写字楼里写字间，写字间里程序员；
+ * 程序人员写程序，又拿程序换酒钱。
+ * 酒醒只在网上坐，酒醉还来网下眠；
+ * 酒醉酒醒日复日，网上网下年复年。
+ * 但愿老死电脑间，不愿鞠躬老板前；
+ * 奔驰宝马贵者趣，公交自行程序员。
+ * 别人笑我忒疯癫，我笑自己命太贱；
+ * 不见满街漂亮妹，哪个归得程序员？
+ * ---------------------------
+ * 项目名： MyUtils
+ * 包名：   com.my.utils.jsonutils
+ * 创建者:  linzhou
+ * 创建时间:18/10/21
+ * 描述:  json-----obj转换工具类
+ *
+ */
+public class JsonUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    /**
+     * 将 POJO 转为 JSON
+     */
+    public static <T> String toJson(T obj) {
+        String json;
+        try {
+            json = OBJECT_MAPPER.writeValueAsString(obj);
+        } catch (Exception e) {
+            LOGGER.error("convert POJO to JSON failure", e);
+            throw new RuntimeException(e);
+        }
+        return json;
+    }
+
+    /**
+     * 将 JSON 转为 POJO
+     */
+    public static <T> T fromJson(String json, Class<T> type) {
+        T pojo;
+        try {
+            pojo = OBJECT_MAPPER.readValue(json, type);
+        } catch (Exception e) {
+            LOGGER.error("convert JSON to POJO failure", e);
+            throw new RuntimeException(e);
+        }
+        return pojo;
+    }
+}
---
+++ src/main/java/com/my/utils/reflectionutils/ReflectionUtil.java
@@ -0,0 +1,104 @@
+package com.my.utils.reflectionutils;
+
+import com.my.utils.classutils.ClassUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * 　　　　　　　　┏┓　　　┏┓+ +
+ * 　　　　　　　┏┛┻━━━┛┻┓ + +
+ * 　　　　　　　┃　　　　　　　┃
+ * 　　　　　　　┃　　　━　　　┃ ++ + + +
+ * 　　　　　　 ████━████ ┃+
+ * 　　　　　　　┃　　　　　　　┃ +
+ * 　　　　　　　┃　　　┻　　　┃
+ * 　　　　　　　┃　　　　　　　┃ + +
+ * 　　　　　　　┗━┓　　　┏━┛
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃ + + + +
+ * 　　　　　　　　　┃　　　┃　　　　Code is far away from bug with the animal protecting
+ * 　　　　　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug
+ * 　　　　　　　　　┃　　　┃
+ * 　　　　　　　　　┃　　　┃　　+
+ * 　　　　　　　　　┃　 　　┗━━━┓ + +
+ * 　　　　　　　　　┃ 　　　　　　　┣┓
+ * 　　　　　　　　　┃ 　　　　　　　┏┛
+ * 　　　　　　　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　　　　　　　┃┫┫　┃┫┫
+ * 　　　　　　　　　　┗┻┛　┗┻┛+ + + +
+ * 佛曰:
+ * 写字楼里写字间，写字间里程序员；
+ * 程序人员写程序，又拿程序换酒钱。
+ * 酒醒只在网上坐，酒醉还来网下眠；
+ * 酒醉酒醒日复日，网上网下年复年。
+ * 但愿老死电脑间，不愿鞠躬老板前；
+ * 奔驰宝马贵者趣，公交自行程序员。
+ * 别人笑我忒疯癫，我笑自己命太贱；
+ * 不见满街漂亮妹，哪个归得程序员？
+ * ---------------------------
+ * 项目名： MyUtils
+ * 包名：   com.my.utils.reflectionutils
+ * 创建者:  linzhou
+ * 创建时间:18/10/21
+ * 描述:  java反射工具类
+ */
+public class ReflectionUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 创建实例（根据类名）
+     */
+    public static Object newInstance(String className) {
+        Class<?> cls = ClassUtil.loadClass(className);
+        return newInstance(cls);
+    }
+
+    /**
+     * 调用方法
+     */
+    public static Object invokeMethod(Object obj, Method method, Object... args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置某个对象的成员变量的值
+     * @param obj    要设置成员变量值的对象
+     * @param field   成员变量的field
+     * @param value  值
+     */
+    public static void setField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
